9f1cd43d7fad7d229efd7dd45f12c43c5a7a9c08
Julian Rodriguez G <julian.rodriguezg@udea.edu.co>
2025-08-27 21:39:22 -0500

    delete unnecesary code

--- src/main/java/co/com/accenture/infrastructure/persistence/mysql/repository/ReactivePersonMongoRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.accenture.infrastructure.persistence.mysql.repository;
-
-import co.com.accenture.infrastructure.persistence.mysql.entity.PersonEntity;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-//import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ReactivePersonMongoRepository extends R2dbcRepository<PersonEntity, Long> {
-}
